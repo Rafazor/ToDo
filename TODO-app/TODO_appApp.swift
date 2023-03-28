a499c93e981435cb175f35b259e13b7d0127e089
Călin Rafa <calin.rafa@alexishr.com>
2023-03-28 20:02:25 +0300

    Initial Commit

---
+++ TODO-app/TODO_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_appApp.swift
+//  TODO-app
+//
+//  Created by Calin Rafa on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TODO_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
